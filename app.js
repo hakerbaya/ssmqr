bafd96c6d642e1a3f15b465fb454303120d1bb36
hakerbaya <hakerbaya786@gmail.com>
2020-08-05 06:22:20 +0530

    Fixed my directory structure

--- app.js
+++ app.js
@@ -31,7 +31,8 @@ app.use(cors());
 // Set Ejs Template Engine
 app.set('view-engine','ejs');
 // Serve Static Files
-app.use(express.static(__dirname));
+// app.use(express.static(__dirname));
+app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
